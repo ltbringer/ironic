986e3b7b9396a8008690724a6ca6b2e88f010847
AmreshVenugopal <amresh.venugopal@gmail.com>
2020-05-17 22:41:35 +0530

    add: neural network struct and some methods, ndarray function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+struct NN {
+    layers: Vec<usize>,
+    weights: Option<Vec<Vec<Vec<f64>>>>,
+    bias: Option<Vec<Vec<f64>>>
+}
+
+
+fn make_nd_array(rows: usize, cols: usize) -> Vec<Vec<f64>> {
+    let mut matrix: Vec<Vec<f64>> = Vec::with_capacity(rows);
+    for _ in 0..matrix.capacity() {
+        let mut array: Vec<f64> = Vec::with_capacity(cols);
+        for _ in 0..array.capacity() {
+            array.push(0.0);
+        }
+        matrix.push(array);
+    }
+    return matrix
+}
+
+impl NN {
+    fn init(&mut self) -> () {
+        let layer_length: &usize = &self.layers.len();
+        let excluding_last: usize = layer_length - 1;
+        let row_slice: &[usize] = &self.layers[..excluding_last];
+        let col_slice: &[usize] = &self.layers[1..];
+        let mut weights: Vec<Vec<Vec<f64>>> = Vec::with_capacity(row_slice.len());
+        for i in 0..weights.capacity() {
+            weights.push(make_nd_array(row_slice[i], col_slice[i]));
+        }
+        self.weights = Some(weights);
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
