8a6e246b288ab0f78c9370d47c2cc5924306e401
AmreshVenugopal <amresh.venugopal@gmail.com>
2020-05-17 23:05:48 +0530

    tests: update weight assignment by shape.

--- src/lib.rs
+++ src/lib.rs
@@ -37,15 +37,23 @@ mod tests {
     use crate::NN;
 
     #[test]
-    fn it_works() {
-        let layers = vec![2, 3, 1];
-        let mut nn = NN { layers, bias: None, weights: None };
-        nn.init();
+    fn are_layers_correct() {
+        let layers: Vec<usize> = vec![2, 3, 1];
+        let nn = NN { layers: &layers, bias: None, weights: None };
         assert_eq!(nn.layers.len(), 3);
+    }
+
+    #[test]
+    fn are_weights_right_shape() {
+        let layers: Vec<usize> = vec![2, 3, 1];
+        let mut nn = NN { layers: &layers, bias: None, weights: None };
+        nn.init();
         match nn.weights {
             Some(w) => {
-                println!("ok {:?}", w);
-                assert_eq!(6, 6);
+                assert_eq!(w.len(), layers[1..].len());
+                assert_eq!(w[0].len(), layers[0]);
+                assert_eq!(w[0][0].len(), layers[1]);
+                assert_eq!(w[1][0].len(), layers[2]);
             },
             None => panic!("Something's wrong I can feel it.")
         }
